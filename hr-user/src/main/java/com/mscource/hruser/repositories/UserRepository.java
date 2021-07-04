712dd65f775e9b1504737485f09d7d549a1191de
Danilo S. Elias <danilo.s.elias@gmail.com>
2021-07-04 16:32:09 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/mscource/hruser/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mscource.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mscource.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+	
+}
